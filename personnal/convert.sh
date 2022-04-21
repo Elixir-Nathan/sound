f30b20be2c774eee4abe3a567e143f6650165386
Elixir-Nathan <nathanael.martin210918@gmail.com>
2022-04-21 22:13:27 +0200

    Passage en MP4 V1

--- personnal/convert.sh
+++ personnal/convert.sh
@@ -1,7 +1,12 @@
 #!bin/bash
-for i in *.mp3
+#for i in *.mp3
+#do
+#	ffmpeg -i "$i" -filter:a "volume=18dB" "${i%%.*}.temp.mp3"
+#	rm "$i"
+#	mv "${i%%.*}.temp.mp3" "$i"
+#done
+
+for i in *.mp4
 do
-	ffmpeg -i "$i" -filter:a "volume=18dB" "${i%%.*}.temp.mp3"
-	rm "$i"
-	mv "${i%%.*}.temp.mp3" "$i"
+	mv "${i%%.*}" "${i%%.*}.mp4"
 done
